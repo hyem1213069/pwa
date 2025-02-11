218e814ba30de53e48df6e66ad2eb4f06c4eaff3
DESKTOP-29VI7GO\user <skfn321@naver.com>
2025-02-11 17:23:46 +0900

    no message

--- javascript_work/0211/test.js
+++ javascript_work/0211/test.js
@@ -1,7 +1,30 @@
-function solution(numbers) {
-    return numbers[0] * numbers[1];
-    numbers.sort((a, b) => b - a);
-}
+// function solution(numbers) {
+//     return numbers[0] * numbers[1];
+//     numbers.sort((a, b) => b - a);
+// }
+//
+// console.log(solution([1, 2, 3, 4, 5]));
+// console.log(solution([0, 31, 24, 10, 1, 9]));
+//
 
-console.log(solution([1, 2, 3, 4, 5]));
-console.log(solution([0, 31, 24, 10, 1, 9]));
+const a = [1,50,100,70,10,200];
+
+const answer = a[0];
+for (let i = 0; i < a.length; i++) {
+    // console.log(a[i]);
+    if (answer < a[i]) {
+        answer = a[i];
+    }
+}
+console.log(answer);
+
+const ret = Math.max(...a);
+console.log(ret);
+
+console.log(a);
+console.log(...a);
+
+const [k,y,y,z,qq] = [...a];
+console.log(k);
+console.log(z);
+console.log(z);
