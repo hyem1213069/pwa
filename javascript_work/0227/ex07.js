490ce33afbc9d1a75a562463780956390a2cfe49
DESKTOP-29VI7GO\user <skfn321@naver.com>
2025-02-27 11:50:40 +0900

    no message

--- javascript_work/0227/ex07.js
+++ javascript_work/0227/ex07.js
@@ -1,3 +1,8 @@
+const obj1 = {}; // 호출 할 수없다
+const obj2 = function(){}; // 호출 할 수 있다
+console.log(`obj2.length ${obj2.length}`);
+console.log(`obj2.length ${obj2.length}`);
+
 const name = '홍길동';
 const incr = function(num) {
     return ++num;
@@ -16,15 +21,30 @@ const auxs = {
 function makeCounter(aux) {
     let num =0;
     return function (){
-        num = aux(num);
+        num = (function (num) { return ++num;})(num);
+        // num = aux(num);
+        return num;
+    }
+}
+
+// const increaser = makeCounter(auxs.incr);
+// const increaser = makeCounter(function(num){return ++num;});
+
+function makeCounter(aux){
+    let num = 0;
+    return function(){
+        num = (function(num){return++num;})(num);
         return num;
     }
 }
 
 const increaser = makeCounter(auxs.incr);
-// const increaser = makeCounter(function(num){return ++num;});
-console.log(increaser);
-console.log(`increaser ${increaser}`);
+console.log(increaser()); //1
+console.log(increaser()); //2
+console.log(increaser()); //3
+
+// console.log(increaser);
+// console.log(`increaser ${increaser}`);
 
 // console.log(incr(10));
 // console.log(auxs.decr(10));
