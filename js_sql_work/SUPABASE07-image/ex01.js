d13624cc45ea09e65e6b5251711ebb3b17be8bc1
DESKTOP-29VI7GO\user <skfn321@naver.com>
2025-03-14 09:39:13 +0900

    no message

---
+++ js_sql_work/SUPABASE07-image/ex01.js
@@ -0,0 +1,14 @@
+const arr = [10,20];
+console.log(arr.pop());
+
+// 제일 마지막 요소 넣기
+// push()
+// 제일 마지막 요소 가져오기
+// pop()
+
+const file = "aa.jpg";
+// ['aa', 'jpg' ]
+console.log(file.split(".").pop());
+
+const uuid = crypto.randomUUID();
+console.log(uuid);
